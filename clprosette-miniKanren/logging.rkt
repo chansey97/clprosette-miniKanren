27db9055ec746255335d07b1f33cd4220c0996d6
Siyuan Chen <chansey97@outlook.com>
2022-03-23 09:58:21 +0800

    Simplify logging.rkt by macro.

--- clprosette-miniKanren/logging.rkt
+++ clprosette-miniKanren/logging.rkt
@@ -1,12 +1,18 @@
 #lang racket/base
 (provide (all-defined-out))
 
-(define (printf/debug x . xs) (void))
-;; (define (printf/debug x . xs) (apply printf (cons x xs)))
+(define-syntax switch-log
+  (syntax-rules ()
+    [(_ printf/debug printf/info printf/warning #t)
+     (begin
+       (define (printf/debug x . xs) (apply printf (cons x xs)))
+       (define (printf/info x . xs) (apply printf (cons x xs)))
+       (define (printf/warning x . xs) (apply printf (cons x xs))))]
+    [(_ printf/debug printf/info printf/warning #f)
+     (begin
+       (define (printf/debug x . xs) (void))
+       (define (printf/info x . xs) (void))
+       (define (printf/warning x . xs) (void)))]))
 
-(define (printf/info x . xs) (void))
-;; (define (printf/info x . xs) (apply printf (cons x xs)))
-
-;; (define (printf/warning x . xs) (void))
-(define (printf/warning x . xs) (apply printf (cons x xs)))
-
+;; (switch-log printf/debug printf/info printf/warning #t)
+(switch-log printf/debug printf/info printf/warning #f)
